e29fce00e55f854946ea0d09ac2813e550d2158c
Igor Tatara <tataraigor@gmail.com>
2023-06-03 19:56:00 +0200

    Dodanie obsługi edycji i usuwania w bazie danych

--- app/src/main/java/com/example/todo/database/SQLHelper.kt
+++ app/src/main/java/com/example/todo/database/SQLHelper.kt
@@ -10,7 +10,7 @@ import java.lang.Exception
 
 class SQLHelper(context:Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
 
-    companion object{
+    companion object {
         private const val DATABASE_VERSION = 1
         private const val DATABASE_NAME = "todo.db"
         private const val TABLE = "events"
@@ -23,7 +23,8 @@ class SQLHelper(context:Context) : SQLiteOpenHelper(context, DATABASE_NAME, null
     }
 
     override fun onCreate(database: SQLiteDatabase?) {
-        val crateTable = ("CREATE TABLE " + TABLE + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + TITLE + " TEXT, " + DESC + " TEXT, " + DATE + " INTEGER, " + PRIO + " INTEGER " + ")")
+        val crateTable =
+            ("CREATE TABLE " + TABLE + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + TITLE + " TEXT, " + DESC + " TEXT, " + DATE + " INTEGER, " + PRIO + " INTEGER " + ")")
         database?.execSQL(crateTable)
     }
 
@@ -32,7 +33,7 @@ class SQLHelper(context:Context) : SQLiteOpenHelper(context, DATABASE_NAME, null
         onCreate(database)
     }
 
-    fun addEvent(std: EventModel): Long{
+    fun addEvent(std: EventModel): Long {
         val database = this.writableDatabase
 
         val data = ContentValues()
@@ -47,15 +48,15 @@ class SQLHelper(context:Context) : SQLiteOpenHelper(context, DATABASE_NAME, null
     }
 
     @SuppressLint("Range")
-    fun displayEvents(): ArrayList<EventModel>{
+    fun displayEvents(): ArrayList<EventModel> {
         val events: ArrayList<EventModel> = ArrayList()
         val query = "SELECT * FROM $TABLE"
         val database = this.readableDatabase
         val cursor: Cursor?
 
-        try{
+        try {
             cursor = database.rawQuery(query, null)
-        } catch (error: Exception){
+        } catch (error: Exception) {
             error.printStackTrace()
             database.execSQL(query)
             return ArrayList()
@@ -67,7 +68,7 @@ class SQLHelper(context:Context) : SQLiteOpenHelper(context, DATABASE_NAME, null
         var date: Int
         var prio: Int
 
-        if(cursor.moveToFirst()) {
+        if (cursor.moveToFirst()) {
             do {
                 id = cursor.getInt(cursor.getColumnIndex("event_id"))
                 title = cursor.getString(cursor.getColumnIndex("event_title"))
@@ -75,10 +76,42 @@ class SQLHelper(context:Context) : SQLiteOpenHelper(context, DATABASE_NAME, null
                 date = cursor.getInt(cursor.getColumnIndex("event_date"))
                 prio = cursor.getInt(cursor.getColumnIndex("event_prio"))
 
-                val event = EventModel(event_id = id, event_title = title, event_desc = desc, event_date = date, event_prio = prio)
+                val event = EventModel(
+                    event_id = id,
+                    event_title = title,
+                    event_desc = desc,
+                    event_date = date,
+                    event_prio = prio
+                )
                 events.add(event)
-            } while(cursor.moveToNext())
+            } while (cursor.moveToNext())
         }
+        database.close()
         return events
+    }
+
+    fun removeEvent(ID: Int) {
+        val database = this.readableDatabase
+        val clause = "$ID = ?"
+        val argument = arrayOf(ID.toString())
+
+        database.delete(TABLE, clause, argument)
+        database.close()
+    }
+
+    fun updateEvent(Event: EventModel) {
+        val database = this.writableDatabase
+
+        val values = ContentValues()
+        values.put(TITLE, Event.event_title)
+        values.put(DESC, Event.event_desc)
+        values.put(DATE, Event.event_date)
+        values.put(PRIO, Event.event_prio)
+
+        val clause = "$ID = ?"
+        val argument = arrayOf(Event.event_id.toString())
+
+        val updatedCount = database.update(TABLE, values, clause, argument)
+        database.close()
     }
 }
